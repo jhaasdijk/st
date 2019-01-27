492f84216b752a11fbc88b83154e8c3c7bd5e7cb
jhaasdijk <jhaasdijk@protonmail.com>
2019-01-27 22:47:02 +0100

    Disable up scrolling printing ^U output to screen

--- config.h
+++ config.h
@@ -170,7 +170,7 @@ static unsigned int defaultattr = 11;
  */
 static MouseShortcut mshortcuts[] = {
 	/* button               mask            string */
-	{ Button4,              XK_ANY_MOD,     "\031" },
+	//{ Button4,              XK_ANY_MOD,     "\031" },
 	{ Button5,              XK_ANY_MOD,     "\005" },
 };
 
