1356c2cf5fa55f4025993c50d6691aac2e3c1406
jhaasdijk <j.haasdijk@student.ru.nl>
2019-04-09 10:38:14 +0200

    Reset to d42e4c49249f94ca29883b28ac06d72b0ccb53ba

--- st.h
+++ st.h
@@ -124,7 +124,7 @@ void drawboxes(XftDraw *, int, int, int, int, XftColor *, const XftGlyphFontSpec
 extern char *utmp;
 extern char *stty_args;
 extern char *vtiden;
-extern char *worddelimiters;
+extern wchar_t *worddelimiters;
 extern int allowaltscreen;
 extern char *termname;
 extern unsigned int tabspaces;
