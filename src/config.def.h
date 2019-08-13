f798c56b2bd1f827cb6bf0e12220383c960a6193
jhaasdijk <j.haasdijk@student.ru.nl>
2019-08-13 18:29:07 +0200

    Decrease font size slightly

--- src/config.def.h
+++ src/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "Ubuntu Mono:size=12:antialias=true:autohint=true";
+char font[] = "Ubuntu Mono:size=11:antialias=true:autohint=true";
 
 /* disable bold, italic and roman fonts globally */
 int disablebold = 1;
