b3f557952f893830da8a2149ca58b7736c44eedc
jhaasdijk <j.haasdijk@student.ru.nl>
2020-05-18 00:05:46 +0200

    Update font settings

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "Ubuntu Mono:size=12:antialias=true:autohint=true";
+char font[] = "JetBrainsMono Nerd Font:size=10:antialias=true:autohint=false";
 
 /* disable bold, italic and roman fonts globally */
 int disablebold = 1;
