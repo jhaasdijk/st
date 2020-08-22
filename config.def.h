574eabc12ba92f10cab3e0018344880e6beb5b30
jhaasdijk <j.haasdijk@student.ru.nl>
2020-08-22 22:54:23 +0200

    Increase borderpx to 15

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ int disablebold = 1;
 int disableitalic = 1;
 int disableroman = 1;
 
-int borderpx = 2;
+int borderpx = 15;
 
 /*
  * What program is execed by st depends of these precedence rules:
